f11717d80dd7f7a37ea27feac80dd47d25cc6604
unknown <mletemps@hotmail.com>
2022-09-24 16:25:55 -0400

    added jest and configuration for testing, still needs unit test cases

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,13 @@
+// .eslintrc.js
+
 module.exports = {
   env: {
     browser: true,
     node: true,
     commonjs: true,
     es2021: true,
+    // https://eslint.org/docs/user-guide/configuring/language-options#specifying-environments
+    jest: true,
   },
   extends: 'eslint:recommended',
   overrides: [],
