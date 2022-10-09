d84f4187d5ad50781b06e804d3033281c2fe666e
unknown <mletemps@hotmail.com>
2022-10-09 14:24:24 -0400

    initial commit with fragments/:id

--- src/routes/api/get.js
+++ src/routes/api/get.js
@@ -11,19 +11,37 @@ function parseJwt(token) {
 /**
  * Get a list of fragments for the current user
  */
-module.exports.fragments = (req, res) => {
-  logger.info('GET v1/fragments: ', req);
+module.exports = {
+  fragments: (req, res) => {
+    logger.info('GET v1/fragments: ', req);
 
-  logger.debug('query: ');
-  logger.debug(req?.query);
+    logger.debug('query: ');
+    logger.debug(req?.query);
 
-  // call the byUser method with the expansion
-  let jwtJSON = parseJwt(req.headers['authorization']);
-  Fragment.byUser(jwtJSON.jti, req?.query.expanded === '1').then((result) => {
-    res.status(200).json(
-      response.createSuccessResponse({
-        fragments: result,
-      })
-    );
-  });
+    // call the byUser method with the expansion
+    let jwtJSON = parseJwt(req.headers['authorization']);
+    Fragment.byUser(jwtJSON.jti, req?.query.expanded === '1').then((result) => {
+      res.status(200).json(
+        response.createSuccessResponse({
+          fragments: result,
+        })
+      );
+    });
+  },
+
+  fragmentId: (req, res) => {
+    logger.info('GET v1/fragments/:id: ', req);
+    logger.debug('params: ');
+    logger.debug(req?.params);
+
+    // call the byUser method with the expansion
+    let jwtJSON = parseJwt(req.headers['authorization']);
+    Fragment.byId(jwtJSON.jti, req?.params.id).then((result) => {
+      res.status(200).json(
+        response.createSuccessResponse({
+          fragments: result,
+        })
+      );
+    });
+  },
 };
--- src/routes/api/post.js
+++ src/routes/api/post.js
@@ -10,26 +10,28 @@ function parseJwt(token) {
 /**
  * Get a list of fragments for the current user
  */
-module.exports.fragments = (req, res) => {
-  let data;
-  let type = req.headers['content-type'];
-  if (!Fragment.isSupportedType(type)) {
-    throw 'Type is not supported.';
-  } else {
-    let stringType = type.split(';')[1];
-    if (stringType) {
-      data = req.body.toString(stringType);
+module.exports = {
+  fragments: (req, res) => {
+    let data;
+    let type = req.headers['content-type'];
+    if (!Fragment.isSupportedType(type)) {
+      throw 'Type is not supported.';
     } else {
-      data = req.body.toString('utf8');
+      let stringType = type.split(';')[1];
+      if (stringType) {
+        data = req.body.toString(stringType);
+      } else {
+        data = req.body.toString('utf8');
+      }
     }
-  }
-  // use jwt to get the origin_jti which will be used as the ownerId
-  let jwtJSON = parseJwt(req.headers['authorization']);
-  logger.debug('jwt jti: ');
-  logger.debug(jwtJSON.jti);
-  let frag = new Fragment({ ownerId: jwtJSON.jti, type: 'text/plain' });
-  frag.setData(data);
-  frag.save().then(() => {
-    res.status(201).json(response.createSuccessResponse({ fragment: frag }));
-  });
+    // use jwt to get the origin_jti which will be used as the ownerId
+    let jwtJSON = parseJwt(req.headers['authorization']);
+    logger.debug('jwt jti: ');
+    logger.debug(jwtJSON.jti);
+    let frag = new Fragment({ ownerId: jwtJSON.jti, type: 'text/plain' });
+    frag.setData(data);
+    frag.save().then(() => {
+      res.status(201).json(response.createSuccessResponse({ fragment: frag }));
+    });
+  },
 };
