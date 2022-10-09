b16a487c4c42c0ce4cdad8b64a485e0bcd5112aa
unknown <mletemps@hotmail.com>
2022-10-09 14:24:36 -0400

    initial commit for put

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -29,7 +29,12 @@ const rawBody = () =>
 // Define our first route, which will be: GET /v1/fragments
 router.get('/fragments', require('./get').fragments);
 
+router.get('/fragments/:id', require('./get').fragmentId);
+
 // Use a raw body parser for POST, which will give a `Buffer` Object or `{}` at `req.body`
 router.post('/fragments', rawBody(), require('./post').fragments);
+
+// Use a raw body parser for POST, which will give a `Buffer` Object or `{}` at `req.body`
+router.put('/fragments', rawBody(), require('./put').fragments);
 
 module.exports = router;
---
+++ src/routes/api/put.js
@@ -0,0 +1,38 @@
+// src/routes/api/get.js
+
+const response = require('../../response');
+const { Fragment } = require('../../model/fragment');
+const logger = require('../../logger');
+
+// https://stackoverflow.com/questions/38552003/how-to-decode-jwt-token-in-javascript-without-using-a-library
+function parseJwt(token) {
+  return JSON.parse(Buffer.from(token.split('.')[1], 'base64').toString());
+}
+/**
+ * Get a list of fragments for the current user
+ */
+module.exports = {
+  fragments: (req, res) => {
+    let data;
+    let type = req.headers['content-type'];
+    if (!Fragment.isSupportedType(type)) {
+      throw 'Type is not supported.';
+    } else {
+      let stringType = type.split(';')[1];
+      if (stringType) {
+        data = req.body.toString(stringType);
+      } else {
+        data = req.body.toString('utf8');
+      }
+    }
+    // use jwt to get the origin_jti which will be used as the ownerId
+    let jwtJSON = parseJwt(req.headers['authorization']);
+    logger.debug('jwt jti: ');
+    logger.debug(jwtJSON.jti);
+    let frag = new Fragment({ ownerId: jwtJSON.jti, type: 'text/plain' });
+    frag.setData(data);
+    frag.save().then(() => {
+      res.status(200).json(response.createSuccessResponse({ fragment: frag }));
+    });
+  },
+};
