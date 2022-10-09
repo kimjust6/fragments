49a4d4e4c644bfddeb42d1b1529cff967ebdcf14
unknown <mletemps@hotmail.com>
2022-10-09 14:07:41 -0400

    added expansion query param

--- src/routes/api/get.js
+++ src/routes/api/get.js
@@ -14,11 +14,12 @@ function parseJwt(token) {
 module.exports.fragments = (req, res) => {
   logger.info('GET v1/fragments: ', req);
 
-  // check if expanded
   logger.debug('query: ');
-  logger.debug(req.query);
+  logger.debug(req?.query);
+
+  // call the byUser method with the expansion
   let jwtJSON = parseJwt(req.headers['authorization']);
-  Fragment.byUser(jwtJSON.jti).then((result) => {
+  Fragment.byUser(jwtJSON.jti, req?.query.expanded === '1').then((result) => {
     res.status(200).json(
       response.createSuccessResponse({
         fragments: result,
