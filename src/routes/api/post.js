e642113ea37a8d5e1b49c5cc79bb2d4348fac98d
unknown <mletemps@hotmail.com>
2022-10-09 13:46:05 -0400

    fixed basic post

--- src/routes/api/post.js
+++ src/routes/api/post.js
@@ -2,6 +2,7 @@
 
 const response = require('../../response');
 const { Fragment } = require('../../model/fragment');
+const logger = require('../../logger');
 
 function parseJwt(token) {
   return JSON.parse(Buffer.from(token.split('.')[1], 'base64').toString());
@@ -10,15 +11,25 @@ function parseJwt(token) {
  * Get a list of fragments for the current user
  */
 module.exports.fragments = (req, res) => {
-  const data = req.body.toString('utf8');
-  if (!Fragment.isSupportedType(req.headers['content-type'])) {
+  let data;
+  let type = req.headers['content-type'];
+  if (!Fragment.isSupportedType(type)) {
     throw 'Type is not supported.';
+  } else {
+    let stringType = type.split(';')[1];
+    if (stringType) {
+      data = req.body.toString(stringType);
+    } else {
+      data = req.body.toString('utf8');
+    }
   }
   // use jwt to get the origin_jti which will be used as the ownerId
-  // console.log('nice: ', JSON.stringify(req.headers['authorization']));
   let jwtJSON = parseJwt(req.headers['authorization']);
+  logger.debug('jwt jti: ');
+  logger.debug(jwtJSON.jti);
   let frag = new Fragment({ ownerId: jwtJSON.jti, type: 'text/plain' });
   frag.setData(data);
-  frag.save();
-  res.status(201).json(response.createSuccessResponse({ fragment: frag }));
+  frag.save().then(() => {
+    res.status(201).json(response.createSuccessResponse({ fragment: frag }));
+  });
 };
