43ace501e886bed338caeb99a14fc30c779da200
unknown <mletemps@hotmail.com>
2022-11-13 22:16:16 -0500

    updated post test

--- tests/unit/post.test.js
+++ tests/unit/post.test.js
@@ -3,10 +3,10 @@ const request = require('supertest');
 const app = require('../../src/app');
 
 // Using a valid username/password pair should give a success result with a .fragments array
-test('add a new fragment', async () => {
+test('#add a new fragment', async () => {
   const res = await request(app)
     .post('/v1/fragments')
-    .set({ 'Content-Type': 'text/plain; charset=utf-8' })
+    .set({ 'Content-Type': 'text/markdown; charset=utf-8' })
     .send('This is a fragment')
     .auth('user1@email.com', 'password1');
   expect(res.statusCode).toBe(201);
